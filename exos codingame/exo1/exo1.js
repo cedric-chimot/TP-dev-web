ef6ad24295f3e9cd820719165a5b2f630101013e
cedric-chimot <106061524+cedric-chimot@users.noreply.github.com>
2022-07-11 14:42:00 +0200

    exo1 codingame

---
+++ exos codingame/exo1/exo1.js
@@ -0,0 +1,35 @@
+let titre = "Rick Asley - Never gonna give you up";
+document.getElementById("titre").innerHTML = titre;
+
+let elmt = document.getElementById("couplet");
+elmt.removeChild(elmt.firstChild);
+let elmt1 = document.getElementById("choeur");
+elmt1.removeChild(elmt1.firstChild);
+let elmt2 = document.getElementById("choeur");
+let elmt3 = document.getElementById("couplet1");
+let elmt4 = document.getElementById("choeur1");
+let refrain = document.getElementById("refrain");
+let refrain1 = document.getElementById("refrain1");
+
+
+/*elmt.firstElementChild.remove();
+elmt.firstChild.remove();
+
+elmt2.firstElementChild.remove();
+elmt2.firstChild.remove();
+
+elmt3.firstElementChild.remove();
+elmt3.firstChild.remove();
+
+elmt4.firstElementChild.remove();
+elmt4.firstChild.remove();
+
+refrain.firstElementChild.remove();
+refrain.firstChild.remove();
+
+refrain1.firstElementChild.remove();
+refrain1.firstChild.remove();*/
+
+
+/*document.getElementById("refrain").firstElementChild.remove();
+document.getElementById("refrain1").firstElementChild.remove();*/
