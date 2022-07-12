71e92d9fdcbf89829c95570b9472247cea4b77bf
cedric-chimot <106061524+cedric-chimot@users.noreply.github.com>
2022-07-12 14:41:33 +0200

    fin exo2 codingame

--- exos codingame/exo2/exo2.js
+++ exos codingame/exo2/exo2.js
@@ -1,3 +1,8 @@
-//$("#paroles .red").removeClass("red");
-
+//utilisation la librairie D3 pour le style
 $('#paroles .red').removeClass('red');
+
+d3.selectAll(".refrain").classed("italic", "true");             
+
+d3.selectAll("p").style("background-color", "#6495ED");
+
+d3.selectAll("p").style("margin-bottom", "0");
