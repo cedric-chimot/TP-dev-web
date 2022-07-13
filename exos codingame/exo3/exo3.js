cb4bc8885794a1b375c3d2302e10da90f2e58b3b
cedric-chimot <106061524+cedric-chimot@users.noreply.github.com>
2022-07-13 22:58:45 +0200

    modifs CV pdf

--- exos codingame/exo3/exo3.js
+++ exos codingame/exo3/exo3.js
@@ -1,9 +1,21 @@
 checkBox = document.getElementById('masquer-paroles').addEventListener('click', event => {
     if (event.target.checked) {
-        //alert("Checkbox checked!");
         $("#paroles").toggle();
+        $('#parole').contents().last().replaceWith(' Afficher les paroles');
     }
-    else {          
+    else {         
         $("#paroles").show();
+        $('#parole').contents().last().replaceWith(' Masquer les paroles');
+    }
+});
+
+checkBox1 = document.getElementById('masquer-refrains').addEventListener('click', event => {
+    if (event.target.checked) {
+        $(".refrain").toggle();
+        $('#refrain').contents().last().replaceWith(' Afficher les refrains');
+    }
+    else {         
+        $(".refrain").show();
+        $('#refrain').contents().last().replaceWith(' Masquer les refrains');
     }
 });
